eac60acfed337322b705abeccbeef8775b895eaa
tinyoko <yasuharu.yokoi.2005@globis.ac.jp>
2024-12-08 19:56:21 +0900

    CSS enable on Local Mac

--- pdf_score/settings.py
+++ pdf_score/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # 静的ファイルの収集先ディレクトリ
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
